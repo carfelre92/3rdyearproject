e9516714dbe4954f128f9f0c805687edb021df5c
Mikey Stewart <mikey.snooker147@gmail.com>
2017-10-10 00:05:01 +1300

    Formatted info page

--- NutritionApp/app/src/main/java/com/google/sample/cloudvision/NutritionInfo.java
+++ NutritionApp/app/src/main/java/com/google/sample/cloudvision/NutritionInfo.java
@@ -3,6 +3,8 @@ package com.google.sample.cloudvision;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
@@ -16,18 +18,106 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import static java.lang.Math.round;
+
 public class NutritionInfo extends AppCompatActivity {
 
     private DatabaseReference dbRef;
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
+    //Reccomended daily intake of certain nutrients
+    //This info is based on the guide provided by the australia new zealand food standards code (FSC)
+    //Guide for an average adult
+    final static double FAT_DI = 70.0; //g
+    final static int CHO_DI = 300; //mg
+    final static int SOD_DI = 2300; //mg
+    final static int POT_DI = 4700; //mg
+    final static int CAR_DI = 310; //g
+    final static double PRO_DI = 50.0; //g
+
     private TextView foodInfo;
+    private ProgressBar loadingIcon;
+
+    private TextView caloriesText;
+    private TextView fatText;
+    private TextView cholesterolText;
+    private TextView sodiumText;
+    private TextView potassiumText;
+    private TextView carbohydratesText;
+    private TextView proteinText;
+
+    private TextView caloriesVal;
+    private TextView fatVal;
+    private TextView cholesterolVal;
+    private TextView sodiumVal;
+    private TextView potassiumVal;
+    private TextView carbohydratesVal;
+    private TextView proteinVal;
+
+    private TextView caloriesDI;
+    private TextView fatDI;
+    private TextView cholesterolDI;
+    private TextView sodiumDI;
+    private TextView potassiumDI;
+    private TextView carbohydratesDI;
+    private TextView proteinDI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition_info);
+
+        //Get all components that will be changed at runtime
         foodInfo = (TextView) findViewById(R.id.foodInfo);
+        loadingIcon = (ProgressBar) findViewById(R.id.loadingIcon);
+
+        caloriesText = (TextView) findViewById(R.id.caloriesText);
+        fatText = (TextView) findViewById(R.id.fatText);
+        cholesterolText = (TextView) findViewById(R.id.cholesterolText);
+        sodiumText = (TextView) findViewById(R.id.sodiumText);
+        potassiumText = (TextView) findViewById(R.id.potassiumText);
+        carbohydratesText = (TextView) findViewById(R.id.carbohydratesText);
+        proteinText = (TextView) findViewById(R.id.proteinText);
+
+        caloriesVal = (TextView) findViewById(R.id.caloriesVal);
+        fatVal = (TextView) findViewById(R.id.fatVal);
+        cholesterolVal = (TextView) findViewById(R.id.cholesterolVal);
+        sodiumVal = (TextView) findViewById(R.id.sodiumVal);
+        potassiumVal = (TextView) findViewById(R.id.potassiumVal);
+        carbohydratesVal = (TextView) findViewById(R.id.carbohydratesVal);
+        proteinVal = (TextView) findViewById(R.id.proteinVal);
+
+        caloriesDI = (TextView) findViewById(R.id.caloriesDI);
+        fatDI = (TextView) findViewById(R.id.fatDI);
+        cholesterolDI = (TextView) findViewById(R.id.cholesterolDI);
+        sodiumDI = (TextView) findViewById(R.id.sodiumDI);
+        potassiumDI = (TextView) findViewById(R.id.potassiumDI);
+        carbohydratesDI = (TextView) findViewById(R.id.carbohydratesDI);
+        proteinDI = (TextView) findViewById(R.id.proteinDI);
+
+        caloriesText.setVisibility(View.GONE);
+        fatText.setVisibility(View.GONE);
+        cholesterolText.setVisibility(View.GONE);
+        sodiumText.setVisibility(View.GONE);
+        potassiumText.setVisibility(View.GONE);
+        carbohydratesText.setVisibility(View.GONE);
+        proteinText.setVisibility(View.GONE);
+
+        caloriesVal.setVisibility(View.GONE);
+        fatVal.setVisibility(View.GONE);
+        cholesterolVal.setVisibility(View.GONE);
+        sodiumVal.setVisibility(View.GONE);
+        potassiumVal.setVisibility(View.GONE);
+        carbohydratesVal.setVisibility(View.GONE);
+        proteinVal.setVisibility(View.GONE);
+
+        caloriesDI.setVisibility(View.GONE);
+        fatDI.setVisibility(View.GONE);
+        cholesterolDI.setVisibility(View.GONE);
+        sodiumDI.setVisibility(View.GONE);
+        potassiumDI.setVisibility(View.GONE);
+        carbohydratesDI.setVisibility(View.GONE);
+        proteinDI.setVisibility(View.GONE);
 
 
         String message = getIntent().getStringExtra("bestRes");
@@ -42,10 +132,53 @@ public class NutritionInfo extends AppCompatActivity {
                 if (dataSnapshot.exists()){
                     FoodInformation foodInformation = dataSnapshot.getValue(FoodInformation.class);
 
-                    System.out.println("Calories: " + foodInformation.calories);
-                    System.out.println("Cholesterol: " + foodInformation.cholesterol);
-                    foodInfo.setText("Welcome " + foodInformation.calories + " " + foodInformation.cholesterol);
+                    caloriesVal.setText(foodInformation.calories);
+                    fatVal.setText(foodInformation.fat);
+                    cholesterolVal.setText(foodInformation.cholesterol);
+                    sodiumVal.setText(foodInformation.sodium);
+                    potassiumVal.setText(foodInformation.potassium);
+                    carbohydratesVal.setText(foodInformation.carbohydrates);
+                    proteinVal.setText(foodInformation.protein);
+
+                    //Calc may not be working correctly
+
+                    //int val1 = Integer.parseInt(foodInformation.fat.replaceAll("[^0-9]", ""));
+                    //int val2 = (val1 / FAT_DI) * 100;
+
+                    caloriesDI.setText("N/A");
+                    //fatDI.setText(Double.toString(round(( FAT_DI) * 100), 1));
+                    //cholesterolDI.setText(foodInformation.cholesterol);
+                    //sodiumDI.setText(foodInformation.sodium);
+                    //potassiumDI.setText(foodInformation.potassium);
+                    //carbohydratesDI.setText(foodInformation.carbohydrates);
+                    //proteinDI.setText(foodInformation.protein);
+
+                    loadingIcon.setVisibility(View.GONE);
+                    caloriesText.setVisibility(View.VISIBLE);
+                    fatText.setVisibility(View.VISIBLE);
+                    cholesterolText.setVisibility(View.VISIBLE);
+                    sodiumText.setVisibility(View.VISIBLE);
+                    potassiumText.setVisibility(View.VISIBLE);
+                    carbohydratesText.setVisibility(View.VISIBLE);
+                    proteinText.setVisibility(View.VISIBLE);
+                    caloriesVal.setVisibility(View.VISIBLE);
+                    fatVal.setVisibility(View.VISIBLE);
+                    cholesterolVal.setVisibility(View.VISIBLE);
+                    sodiumVal.setVisibility(View.VISIBLE);
+                    potassiumVal.setVisibility(View.VISIBLE);
+                    carbohydratesVal.setVisibility(View.VISIBLE);
+                    proteinVal.setVisibility(View.VISIBLE);
+                    caloriesDI.setVisibility(View.VISIBLE);
+                    fatDI.setVisibility(View.VISIBLE);
+                    cholesterolDI.setVisibility(View.VISIBLE);
+                    sodiumDI.setVisibility(View.VISIBLE);
+                    potassiumDI.setVisibility(View.VISIBLE);
+                    carbohydratesDI.setVisibility(View.VISIBLE);
+                    proteinDI.setVisibility(View.VISIBLE);
+
+                    //foodInfo.setText("Welcome " + foodInformation.calories + " " + foodInformation.cholesterol);
                 } else {
+                    loadingIcon.setVisibility(View.GONE);
                     foodInfo.setText("No nutrition information found for this food.");
                 }
             }
